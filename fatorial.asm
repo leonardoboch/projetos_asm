7cb385b882c851987dea828f5ba007355a2f435e
Leonardo Bochnia <bochnialeonardo@gmail.com>
2022-06-30 17:37:44 -0300

    Adicionado bloco de recursao

--- fatorial.asm
+++ fatorial.asm
@@ -3,22 +3,34 @@ section	.text
 	
 _start:                 
 
-   mov bx, 3             ;inserir valor para fatorial
-   call  proc_fact
-   add   ax, 30h
-   mov  [fact], ax
+    mov bx, 3             ;inserir valor para fatorial
+    call  _recursao_fatorial
+    add   ax, 30h
+    mov  [fatorial], ax
     
-   mov	  edx,len        ; imprime msg
-   mov	  ecx,msg        
-   mov	  ebx,1          
-   mov	  eax,4          
-   int	  0x80           
+    mov	  edx, len        ; imprime msg
+    mov	  ecx, msg        
+    mov	  ebx, 1          
+    mov	  eax, 4          
+    int	  0x80           
 
-   mov    edx,1            ;imprime num
-   mov	  ecx,fact       
-   mov	  ebx,1          
-   mov	  eax,4          
-   int	  0x80           
+    mov    edx, 1            ;imprime num
+    mov	  ecx, fatorial      
+    mov	  ebx, 1          
+    mov	  eax, 4          
+    int	  0x80           
     
-   mov	  eax,1          
-   int	  0x80           
+    mov	  eax,1          
+    int	  0x80  
+
+
+_recursao_fatorial:
+    cmp   bl, 1
+    jg    _calculo_fat
+    mov   ax, 1
+    ret
+
+_calculo_fat:
+
+
+         
